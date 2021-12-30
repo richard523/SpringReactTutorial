b91b6b614e0586867ac7e0be9970689cb8c0456c
richard523 <60868589+richard523@users.noreply.github.com>
2021-12-30 03:56:50 -0600

    reinstalling node modules

--- src/main/java/com/richard/demo/payroll/ReactAndSpringDataRestApplication.java
+++ src/main/java/com/richard/demo/payroll/ReactAndSpringDataRestApplication.java
@@ -1,12 +1,15 @@
 package com.richard.demo.payroll;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ReactAndSpringDataRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+		
 	}
 }
